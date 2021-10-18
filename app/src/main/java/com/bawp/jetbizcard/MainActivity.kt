42aa9174203b7553df1f9032f6c4d9f05bcc9988
pdichone <pdichone@gmail.com>
2021-10-18 09:33:36 -0700

    Newest one

--- app/src/main/java/com/bawp/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/bawp/jetbizcard/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
             JetBizCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Android, me here!")
                 }
             }
         }
