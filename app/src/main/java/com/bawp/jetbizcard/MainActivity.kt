d3cc9a083d84076ffb9227843d2b69a3b1e21739
pdichone <pdichone@gmail.com>
2021-10-18 11:08:13 -0700

    Surface and Card

--- app/src/main/java/com/bawp/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/bawp/jetbizcard/MainActivity.kt
@@ -3,11 +3,18 @@ package com.bawp.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bawp.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +24,7 @@ class MainActivity : ComponentActivity() {
             JetBizCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android, me here!")
+                    CreateBizCard()
                 }
             }
         }
@@ -25,14 +32,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard() {
+    Surface( modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp) {
+            
+        }
+
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetBizCardTheme {
-        Greeting("Android")
+       CreateBizCard()
     }
 }
